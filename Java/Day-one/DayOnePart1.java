be33dbdc9f6d7e254777ca2ccb126711e4361dd0
Bethy Diakabana <bethyd123@gmail.com>
2016-05-18 23:37:48 -0400

    Create DayOnePart1.java
    added java implementation of day one

---
+++ Java/Day-one/DayOnePart1.java
@@ -0,0 +1,13 @@
+package net.bethydiakabana.solutions.dayone;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class DayOnePart1 {
+	public static int calculateFloors() {
+		int floors = 0;
+		return floors;
+	}
+
+}
